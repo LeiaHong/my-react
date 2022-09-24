26f3a55e576e40ed158ca41d8267604c0e3a11c8
LeiaHong <cattleya198728@gmail.com>
2022-09-24 16:53:03 +0800

    feat: CounterV2

---
+++ src/features/counterV2/CounterV2.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CounterV2 = () => {
+  return (
+    <div class="container">
+  <div class="chevron chevron-up"></div>
+  <div class="number">
+    256
+  </div>
+  <div class="chevron chevron-down"></div>
+</div>
+  )
+}
+
+export default CounterV2
