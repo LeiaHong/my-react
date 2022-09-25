93a80829a40e29a12bda466048f06c227b71b139
LeiaHong <cattleya198728@gmail.com>
2022-09-25 18:28:52 +0800

    一次使用大量計數器

--- src/App.js
+++ src/App.js
@@ -3,9 +3,21 @@ import './App.css';
 import CounterV2 from './features/counterV2/CounterV2';
 import useFirebase from './hooks/useFirebase';
 
+const counters = Array.from({ length: 14 }, (_, index) => index);
 function App() {
   useFirebase();
-  return <CounterV2 />;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexWrap: 'wrap',
+      }}
+    >
+      {counters.map(() => (
+        <CounterV2 />
+      ))}
+    </div>
+  );
 }
 
 export default App;
